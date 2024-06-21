81bb9bfd3084306a6d4102846897060be9939910
TimiDev19 <rotimioluwatimilehin19@gmail.com>
2024-06-21 09:01:49 -0700

    opening animation added

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,38 @@ import { BrowserRouter } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Sidebar from './components/Sidebar';
+import { useEffect, useState } from 'react';
 
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 4000)
+  })
 
   return (
     <div className=' bg-[#F5F5F5] h-[100vh] w-[100vw] overflow-y-scroll'>
-      <BrowserRouter>
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      {
+        isLoading ? (
+          <div className=' h-full w-full flex items-center justify-center'>
+            <div className=' flex items-baseline justify-center h-fit'>
+              <div className=' h-[25px] w-[25px] mx-4 bg-black rounded-full ball'></div>
+            <h1 className=' text-3xl font-thin'>&lt; Oluwatimilehin /&gt;</h1>
+            </div>
+            
+          </div>
+        ) : (
+          <BrowserRouter>
+            <Sidebar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </BrowserRouter>
+        )
+      }
+
     </div>
   )
 }
